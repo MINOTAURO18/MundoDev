1b062e412e47526a6562eefa61c986abc5bcd85a
alexander muñoz <alexander12345my@gmail.com>
2023-04-21 15:54:53 -0500

    about

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,6 +1,8 @@
 import styles from '../Inicio/Inicio.module.css'
+import mapa from '../../assets/mapa.webp'
 const Inicio = () => {
     return(
+  <main>  
       <section className={styles.inicio}>
         <article className={styles.bloqueOne}>
           <h1><span>MundoDev</span> tu fuente de información mas confiable</h1>
@@ -21,13 +23,13 @@ const Inicio = () => {
     </div>
 
     <div className={styles.code}>
-      <h4><span>></span> Install https://MundoDev/insatll.sh | sh</h4>
+      <h4><span>~</span> Install https://MundoDev/insatll.sh | sh</h4>
       <h4><span className={styles.dolar}>$</span> curl -L https://minotauro18.io/MundoDev/.sh | sh</h4>
       <h4 className={styles.eli}>vacio</h4>
-      <h4><span>></span> Ship a Docker image</h4>
+      <h4><span>~</span> Ship a Docker image</h4>
       <h4><span className={styles.dolar}>$</span> MundoDevtl deploy</h4>
       <h4 className={styles.eli}>vacio</h4>
-      <h4><span>></span> Run ir on three Continents</h4>
+      <h4><span>~</span> Run ir on three Continents</h4>
       <h4><span className={styles.dolar}>$</span> MundoDevtl regions add ams hkg sjc</h4> 
     </div>
 
@@ -35,6 +37,24 @@ const Inicio = () => {
 
 
   </section>
+
+
+
+  <section className={styles.sobre}>
+    <div className={styles.sobreN}>
+    <h3>Distribuidores de Hardware</h3>
+    <h2>Quienes somos</h2>
+    <p>Nosotros somos una empresa especializada en la venta de cursos y hardware provenientes de diversas partes del mundo. Nos enfocamos en brindar soluciones y productos de calidad a nuestros clientes, con el objetivo de satisfacer sus necesidades y superar sus expectativas. Ya sea que estén buscando ampliar su conocimiento en un tema específico o adquirir equipos de última generación, estamos comprometidos en ofrecerles las mejores opciones y un servicio excepcional.</p>
+     
+    </div>
+
+    <div className={styles.mapa}>
+      <img src={mapa} alt="mapa" />
+
+    </div>
+
+  </section>
+  </main>
     )
 }
 
